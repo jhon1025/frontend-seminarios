2ebfa1fcfd401f339f475cfba814718f4b354e91
Jhon Murcia <132969460+jhon1025@users.noreply.github.com>
2025-09-02 20:59:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   const el = sel => document.querySelector(sel)
   const els = sel => Array.from(document.querySelectorAll(sel))
-  const state = { editingId: null, apiBase: localStorage.getItem('https://backendseminario-1hmu.onrender.com') || 'https://backend-seminarios.onrender.com' }
+  const state = { editingId: null, apiBase: localStorage.getItem('https://backend-seminarios.onrender.com') || 'https://backend-seminarios.onrender.com' }
 
   const inputId = el('#id')
   const inputTitulo = el('#titulo')
